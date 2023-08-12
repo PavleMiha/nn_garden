2e67d88303a1bc3d4dc59dc3a24be5b94ab5a38c
Pavle Mihajlovic <miha.pavle@gmail.com>
2023-08-12 19:46:19 +0100

    Added imnode example

---
+++ include/node_editor.h
@@ -0,0 +1,8 @@
+#pragma once
+
+namespace example
+{
+void NodeEditorInitialize();
+void NodeEditorShow(float delta);
+void NodeEditorShutdown();
+} // namespace example
--- src/main.cpp
+++ src/main.cpp
@@ -7,18 +7,29 @@
 #include "bigg.hpp"
 #include "value.h"
 #include "imgui.h"
+#include <imnodes.h>
+#include "node_editor.h"
 
 class NNGarden : public bigg::Application
 {
-	void onReset()
-	{
+	void initialize(int _argc, char** _argv) {
+		ImNodes::CreateContext();
+		example::NodeEditorInitialize();
+	}
+	void onReset() {
 		bgfx::setViewClear( 0, BGFX_CLEAR_COLOR | BGFX_CLEAR_DEPTH, 0x303841ff, 1.0f, 0 );
 		bgfx::setViewRect( 0, 0, 0, uint16_t( getWidth() ), uint16_t( getHeight() ) );
 	}
 
-	void update( float dt )
-	{
+	void update( float dt ) {
 		bgfx::touch( 0 );
+
+		//ImGui::NewFrame();
+
+		example::NodeEditorShow(dt);
+
+		// Rendering
+		//ImGui::Render();
 		ImGui::ShowDemoWindow();
 	}
 public:
@@ -26,8 +37,7 @@ public:
 		: bigg::Application( "NNGarden" ) {}
 };
 
-int main( int argc, char** argv )
-{
+int main( int argc, char** argv ) {
 	NNGarden app;
 	return app.run( argc, argv );
 }
