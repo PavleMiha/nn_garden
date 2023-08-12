d6f5741320ef8aabd7c689d36eac48733b1ebed6
Pavle Mihajlovic <miha.pavle@gmail.com>
2023-08-12 17:46:11 +0100

    fix imgui .cmake

--- deps/imgui.cmake
+++ deps/imgui.cmake
@@ -5,6 +5,7 @@ add_library(
 	${CMAKE_CURRENT_LIST_DIR}/imgui/imgui_demo.cpp
 	${CMAKE_CURRENT_LIST_DIR}/imgui/imgui_draw.cpp
 	${CMAKE_CURRENT_LIST_DIR}/imgui/imgui_widgets.cpp
+	${CMAKE_CURRENT_LIST_DIR}/imgui/imgui_tables.cpp
 )
 target_include_directories( imgui PUBLIC ${CMAKE_CURRENT_LIST_DIR}/imgui )
 set_target_properties( imgui PROPERTIES FOLDER "imgui" )
